8f32e53f9c9e22dc12fa4127101ee89287c79a4b
testingasservice2025 <testingasservice@gmail.com>
2025-07-28 17:48:11 -0600

    agregando src/test/java/pages/tc_44Page.java

---
+++ src/test/java/pages/tc_44Page.java
@@ -0,0 +1,43 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class tc_44Page {
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+    public tc_44Page(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+
+    public void navegarPantallaFinalRevisionDocumentos() {
+        // Implementación de navegación a la pantalla final de revisión de documentos
+    }
+
+    public void revisarDocumentosYInformacionExtraida() {
+        // Implementación para revisión de documentos e información extraída
+    }
+
+    public void verificarResumenCompleto() {
+        // Implementación para verificar el resumen completo
+    }
+
+    public void confirmarInformacionYProceder() {
+        // Implementación para confirmar información y proceder
+    }
+
+    public void verificarSolicitudConfirmacionFinal() {
+        // Implementación para verificar solicitud de confirmación final
+    }
+
+    public void aceptarConfirmacionYEnviarSolicitud() {
+        // Implementación para aceptar confirmación y enviar solicitud
+    }
+
+    public void verificarRedireccionSiguientePaso() {
+        // Implementación para verificar la redirección al siguiente paso
+    }
+}
